871120fbe08953cc9fa9b0cdb3085d2971c8abe8
joalira00 <joao134.jol@gmail.com>
2025-03-05 23:51:20 -0300

    added one more line for lint

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ todos_model.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 
+
 @app.get("/scalar", include_in_schema=False)
 async def scalar_html():
     return get_scalar_api_reference(
