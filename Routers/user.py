b453c7a43dde5377a29c713a385ceee9cbc38bf3
joalira00 <joao134.jol@gmail.com>
2025-03-08 15:32:18 -0300

    linted file

--- Routers/user.py
+++ Routers/user.py
@@ -62,11 +62,11 @@ async def recover_password(user_verification: UserVerification,
 async def update_phone_number(user: user_dependency,
                               db: db_dependency,
                               user_new_phone: str = Path(min_length=11,
-                                                          max_length=11)):
-    
+                                                         max_length=11)):
+
     if user is None:
         raise HTTPException(status_code=401, detail="Authentication failed")
-    
+
     user_model = db.query(Users).filter(Users.id == user.get('id')).first()
 
     user_model.phone_number = user_new_phone
